028b69d653f2ed255444f72aff594b68a0004642
Andrés Ariza <andres-ariza70@hotmail.com>
2023-02-08 11:10:43 -0500

    CSS Service added

--- src/main/java/org/arep/webapps/FormCSSService.java
+++ src/main/java/org/arep/webapps/FormCSSService.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class FormService implements RESTService {
+public class FormCSSService implements RESTService {
     @Override
     public String getHeader() {
         return "HTTP/1.1 200 OK\r\n" +
-                "Content-type: text/html\r\n" +
+                "Content-type: text/css\r\n" +
                 "\r\n";
     }
 
@@ -18,7 +18,7 @@ public class FormService implements RESTService {
     public String getResponse() {
         byte[] fileContent;
         try {
-            fileContent = Files.readAllBytes(Paths.get("src/main/resources/form.html"));
+            fileContent = Files.readAllBytes(Paths.get("src/main/resources/style.css"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/arep/webapps/WebApp.java
+++ src/main/java/org/arep/webapps/WebApp.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 public class WebApp {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.getInstance();
-        server.addService("/form", new FormService());
+        server.addService("/form", new FormHTMLService());
+        server.addService("/style", new FormCSSService());
         server.run(args);
     }
 }
